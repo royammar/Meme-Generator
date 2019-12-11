8a35f01abb729387fce3f6a189da297a23009e4d
Roy Amar <royammar030783@gmail.com>
2019-12-11 20:24:59 +0200

    Wednesday

--- js/memeController.js
+++ js/memeController.js
@@ -1,8 +1,98 @@
 'use strict'
 
-var gKeywords = { 'happy': 12, 'funny puk': 1 }
-var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
-var gMeme = {
-    selectedImgId: 5, selectedTxtIdx: 0,
-    txts: [{ line: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }]
-}  
+
+function init() {
+    gCanvas = document.getElementById('canvas-container');
+    gCanvas.width = window.innerWidth - 50
+    gCanvas.height = window.innerHeight - 100;
+    gCtx = gCanvas.getContext('2d')
+    renderImgGallery()
+    window.addEventListener('resize',
+        function () {
+            gCanvas = document.getElementById('canvas-container');
+            gCanvas.width = window.innerWidth - 50
+            gCanvas.height = window.innerHeight - 100;
+        })
+}
+
+
+function renderImg() {
+
+    var gImg = new Image();
+    gImg.src = gCurrImg
+    gImg.onload = () => {
+        gCtx.drawImage(gImg, 0, 0)
+        for (var i = 0; i < gMeme.txts.length; i++) {
+            var txt = getTextToRender(i)
+            var fontSize = getFontSize(i) + "px IMPACT"
+            var xPos = getlineX(i)
+            var yPos = getlineY(i)
+            gCtx.font = fontSize;
+            gCtx.fillStyle = 'white'
+            if (gCurrLine === i) {
+                drawTextBG(gCtx, txt, getFontSize(i), xPos, yPos)
+            }
+            gCtx.fillText(txt, xPos, yPos);
+            gCtx.strokeText(txt, xPos, yPos);
+        }
+    }
+}
+
+function onTextInput() {
+    var txt = document.querySelector('.input').value
+    setMemText(txt)
+    renderImg()
+}
+
+function renderImgGallery() {
+    var strHTML = gImgs.map(function (img) {
+        return `<img src=${img.url} class="img" onclick="onImgSelceted(${img.id})">`
+    })
+    var elGallary = document.querySelector('.gallery')
+    console.log(elGallary);
+
+    elGallary.innerHTML = strHTML.join('')
+}
+
+function onImgSelceted(imgId) {
+    toggleCanvas()
+    gCurrImg = getImgToRender(imgId)
+    setcurrImgId(imgId)
+    renderImg()
+}
+
+function toggleCanvas() {
+    var elCanvas = document.querySelector(".main-container");
+    if (elCanvas.style.display === "none") {
+        elCanvas.style.display = "block";
+    } else {
+        elCanvas.style.display = "none";
+    }
+
+    var elGallary = document.querySelector(".gallery");
+    if (elGallary.style.display === "grid") {
+        elGallary.style.display = "none";
+    } else {
+        elGallary.style.display = "grid";
+    }
+}
+
+
+function onchangeTextSize(diff) {
+    updateFontSize(diff)
+    renderImg()
+}
+
+function onMoveLine(diff) {
+    udpatelineLocation(diff)
+    renderImg()
+}
+
+function onChangeLine() {
+    ClearTextBg(gCtx, getTextToRender(gCurrLine), getFontSize(gCurrLine), getlineX(gCurrLine), getlineY(gCurrLine))
+    updateCurrLine()
+    document.querySelector('.input').value = gMeme.txts[gCurrLine].line
+    renderImg()
+}
+
+
---
+++ js/memeService.js
@@ -0,0 +1,113 @@
+'use strict'
+
+var gCanvas = ''
+var gCtx = ''
+var gCurrImg = ''
+var gid = 0
+var gLineSwitcher = 1
+
+var gCurrLine = 0
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+var gMeme = {
+    selectedImgId: 0, selectedTxtIdx: 0,
+    txts: [{ line: '', size: 20, align: 'left', color: 'white', Xpos: 50, Ypos: 20 },
+    { line: '', size: 20, align: 'left', color: 'white', Xpos: 50, Ypos: 430 }]
+}
+var gImgs = [{ id: 0, url: 'img/0.jpg', keywords: ['Funny'] },
+{ id: 1, url: 'img/1.jpg', keywords: ['Cute', 'Puppy'] },
+{ id: 2, url: 'img/2.jpg', keywords: ['Cute', 'Puppy', 'Baby'] },
+{ id: 3, url: 'img/3.jpg', keywords: ['Baby', 'Funny'] },
+{ id: 4, url: 'img/4.jpg', keywords: ['Cute', 'Cat'] },
+{ id: 5, url: 'img/5.jpg', keywords: ['Funny'] },
+{ id: 6, url: 'img/6.jpg', keywords: ['Funny', 'Baby'] },
+{ id: 7, url: 'img/7.jpg', keywords: ['Funny'] },
+{ id: 8, url: 'img/8.jpg', keywords: ['Funny'] },
+{ id: 9, url: 'img/9.jpg', keywords: ['Funny', 'Baby'] },
+{ id: 10, url: 'img/10.jpg', keywords: ['Funny'] },
+{ id: 11, url: 'img/11.jpg', keywords: ['Funny', 'Sport'] },
+{ id: 12, url: 'img/12.jpg', keywords: ['Happy', 'DiCAaprio'] },
+{ id: 13, url: 'img/13.jpg', keywords: ['Scarry'] },
+{ id: 14, url: 'img/14.jpg', keywords: ['Funny'] },
+{ id: 15, url: 'img/15.jpg', keywords: ['Funny'] },
+{ id: 16, url: 'img/16.jpg', keywords: ['Scarry'] },
+{ id: 17, url: 'img/17.jpg', keywords: ['Funny', 'Cartoon'] },
+];
+
+
+function getImgToRender(imgId) {
+    return gImgs[imgId].url
+}
+
+
+function getTextToRender(lineIndex) {
+
+    return gMeme.txts[lineIndex].line
+}
+
+
+function setMemText(txt) {
+    gMeme.txts[gCurrLine].line = txt
+}
+
+function setcurrImgId(imgId) {
+    gMeme.selectedImgId = imgId
+}
+
+
+function updateFontSize(diff) {
+    gMeme.txts[gCurrLine].size += diff
+}
+
+function getFontSize(lineIndex) {
+    return gMeme.txts[lineIndex].size
+}
+
+function udpatelineLocation(diff) {
+
+    gMeme.txts[gCurrLine].Ypos += diff
+}
+
+
+function getlineX(lineIndex) {
+
+    return gMeme.txts[lineIndex].Xpos
+
+
+}
+
+function getlineY(lineIndex) {
+    return gMeme.txts[lineIndex].Ypos
+}
+
+
+function updateCurrLine() {
+    gCurrLine += gLineSwitcher
+    gLineSwitcher *= -1
+}
+
+
+
+
+function drawTextBG(gCtx, txt, font, x, y) {
+    gCtx.save();
+    gCtx.font = font;
+    gCtx.textBaseline = 'center';
+    gCtx.fillStyle = '#f50';
+    var width = gCtx.measureText(txt).width;
+    gCtx.beginPath();
+    gCtx.rect(x, y - font + 4, width + 2, parseInt(font, 10));
+    gCtx.fillStyle = '#000';
+    gCtx.stroke()
+    gCtx.closePath()
+    gCtx.restore();
+}
+
+
+function ClearTextBg(gCtx, txt, font, x, y) {
+    gCtx.font = font;
+    gCtx.textBaseline = 'center';
+    var width = gCtx.measureText(txt).width;
+    gCtx.clearRect(x, y - font + 4, width + 2, parseInt(font, 10));
+}
+
+
