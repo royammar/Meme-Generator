7dced0eb97dc5555ee1ecdb0ff16be6ced12a8d8
Roy Amar <royammar030783@gmail.com>
2019-12-11 10:34:29 +0200

    first commit

---
+++ js/memeController.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
+var gMeme = {
+    selectedImgId: 5, selectedTxtIdx: 0,
+    txts: [{ line: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }]
+}  
